d322a246b84810ddce3ecb4e76db117fbf9a50b5
Wachu1 <dominik.wach06@gmail.com>
2024-03-10 15:21:33 +0100

    1.0

---
+++ assets/script/product.js
@@ -0,0 +1,112 @@
+const products = [
+    {
+        id: 0,
+        name: "Sony A1",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Ipsam nemo amet saepe expedita velit, praesentium voluptatum nobis
+        provident atque sapiente totam odio.
+        Dolorum, at. Magni quos modi a fugit atque?`,
+        category: "Sony",
+        price: 31999.99,
+        image: "./assets/images/sony-A1.jpg",
+        sale: true,
+        saleAmount: 12000,
+    },
+    {
+        id: 1,
+        name: "Sony A6700",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Ipsam nemo amet saepe expedita velit, praesentium voluptatum nobis
+        provident atque sapiente totam odio.
+        Dolorum, at. Magni quos modi a fugit atque?`,
+        category: "Sony",
+        price: 7899.99,
+        image: "./assets/images/sony-A6700.jpg",
+    },
+    {
+        id: 2,
+        name: "Sony FX3",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Ipsam nemo amet saepe expedita velit, praesentium voluptatum nobis
+        provident atque sapiente totam odio.
+        Dolorum, at. Magni quos modi a fugit atque?`,
+        category: "Sony",
+        price: 21999.99,
+        image: "./assets/images/sony-FX3.jpg",
+        sale: true,
+        saleAmount: 299,
+    },
+    {
+        id: 3,
+        name: "Sony A7IV",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Ipsam nemo amet saepe expedita velit, praesentium voluptatum nobis
+        provident atque sapiente totam odio.
+        Dolorum, at. Magni quos modi a fugit atque?`,
+        category: "Sony",
+        price: 13499.99,
+        image: "./assets/images/sony-A7IV.jpg",
+    },
+    {
+        id: 4,
+        name: "Canon R5",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Ipsam nemo amet saepe expedita velit, praesentium voluptatum nobis
+        provident atque sapiente totam odio.
+        Dolorum, at. Magni quos modi a fugit atque?`,
+        category: "Canon",
+        price: 15999.99,
+        image: "./assets/images/canon-r5.jpg",
+    },
+    {
+        id: 5,
+        name: "Canon R5C",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Ipsam nemo amet saepe expedita velit, praesentium voluptatum nobis
+        provident atque sapiente totam odio.
+        Dolorum, at. Magni quos modi a fugit atque?`,
+        category: "Canon",
+        price: 24999.99,
+        image: "./assets/images/canon-r5c.jpeg",
+        sale: true,
+        saleAmount: 5000,
+    },
+    {
+        id: 6,
+        name: "Canon R10",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Ipsam nemo amet saepe expedita velit, praesentium voluptatum nobis
+        provident atque sapiente totam odio.
+        Dolorum, at. Magni quos modi a fugit atque?`,
+        category: "Canon",
+        price: 5999.99,
+        image: "./assets/images/canon-r10.jpeg",
+    },
+    {
+        id: 7,
+        name: "Lumix S1",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Ipsam nemo amet saepe expedita velit, praesentium voluptatum nobis
+        provident atque sapiente totam odio.
+        Dolorum, at. Magni quos modi a fugit atque?`,
+        category: "Lumix",
+        price: 11999.99,
+        image: "./assets/images/lumix-S1.jpg",
+        sale: true,
+        saleAmount: 1200,
+    },
+    {
+        id: 8,
+        name: "Lumix S5 mark 2",
+        description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Ipsam nemo amet saepe expedita velit, praesentium voluptatum nobis
+        provident atque sapiente totam odio.
+        Dolorum, at. Magni quos modi a fugit atque?`,
+        category: "Lumix",
+        price: 13499.99,
+        image: "./assets/images/lumix-S5M2.jpeg",
+        sale: true,
+        saleAmount: 1200,
+    },
+
+];
